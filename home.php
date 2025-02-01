1b70bc36c89882e9a895cbcabb33f89fa3682806
eeldin <ek70543@ubt-uni.net>
2025-02-01 20:15:11 +0100

    commit

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
 
     <section class="ourproducts">
         <h1>Disa nga produktet tona</h1>
-
+        
         <div class="slider">
             <div class="slider-container" id="slider-container">
                 
@@ -26,34 +26,6 @@
             <a href="Products.html" class="continue">
                 <button >Vazhdoni tek faqja e produkteve</button>
             </a>
-        </div>
-
-        <div class="product-container-1">
-            <a href="Products/product1.html" class="product-item">
-                <img src="assets/product1.jpg" alt="Product 1">
-                <p>Initio Side Effect 90ml</p>
-            </a>
-            <a href="Products/product2.html" class="product-item">
-                <img src="assets/product2.jpg" alt="Product 2">
-                <p>Eau De Parfum Xerjoff "V" Erba Pura, 100 ml</p>
-            </a>
-            <a href="Products/product3.html" class="product-item">
-                <img src="assets/product3.jpg" alt="Product 3">
-                <p>Jean Paul Gaultier Ultra Male 125 ml</p>
-            </a>
-            <a href="Products/product4.html" class="product-item">
-                <img src="assets/product4.jpg" alt="Product 4">
-                <p>Jean Paul Gaultier Ultra Male 125 ml</p>
-            </a>
-            <a href="Products/product5.html" class="product-item">
-                <img src="assets/product5.jpg" alt="Product 5">
-                <p>Jean Paul Gaultier Ultra Male 125 ml</p>
-            </a>
-            <a href="Products/product6.html" class="product-item">
-                <img src="assets/product6.jpg" alt="Product 6">
-                <p>Jean Paul Gaultier Ultra Male 125 ml</p>
-            </a>
-        </div>
     </section>
 
     <?php include 'footer.php'; ?>
